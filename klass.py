7bbeb1360d46bdd0e23e643dbe01d612e88533b3
Dema267 <demademin292@gmail.com>
2024-12-06 15:00:55 +0300

    add training1

--- klass.py
+++ klass.py
@@ -1,16 +1,13 @@
 class Warrior ():
     def __init__(self, name, power, endurance, hair_color):
         self.name = name
-
         self.power = power
-
         self.endurance = endurance
-
         self.hair_color = hair_color
 
     def sleep(self):
             print(f"{self.name} лег спать")
-            self.endurance = + 2
+            self.endurance =+ 2
 
 
     def eat(self):
@@ -29,3 +26,10 @@ class Warrior ():
             print(f"цвет волос воина - {self.hair_color}")
             print(f"сила воина - {self.power}")
             print(f"выносливость воина - {self.endurance}")
+
+war1 = Warrior(name= "Степа", power=76, endurance=54, hair_color="коричневый")
+war2 = Warrior(name= "Егор", power=23, endurance=34, hair_color="блонд")
+
+print(war2.endurance)
+war2.sleep()
+print(war2.endurance)
